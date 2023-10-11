6c23c80f10b4671a0a783411258671a5caba900b
Pedro Henrique Kaspchark <pkaspchark@gmail.com>
2023-10-10 22:13:30 -0300

    Criando os métodos e o service de usuario

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Message, MessageService } from 'primeng/api';
 import { User } from 'src/app/interfaces/user';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
     selector: 'app-login',
@@ -12,8 +13,27 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 })
 export class LoginComponent implements OnInit {
     public valCheck: string[] = ['remember'];
+    public user!: User;
+    public form!: FormGroup;
 
-    constructor(public layoutService: LayoutService, private router: Router) {}
+    constructor(public layoutService: LayoutService, private router: Router, private formbuilder: FormBuilder, private authService: AuthService, private messageService: MessageService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.initializeForm();
+    }
+
+    initializeForm(){
+        this.form = this.formbuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]]
+        })
+    }
+
+    login(){
+        const user: User = this.form.value;
+        if(!user){
+            return;
+        }
+        this.authService.login(user).then(() => this.router.navigate(['home'])).catch((error) => {console.log(error); this.messageService.add({severity: 'error', summary: 'Erro de Login', detail: 'Falha na autenticação.'})});
+    }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import { User } from '../interfaces/user';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(auth: Auth) { }
+  constructor(private auth: Auth) { }
 
-  login({email, password}){
-    
+  login({email, password}: User){
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  register({email, password}: User){
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  logout(){
+    return signOut(this.auth);
   }
 }
